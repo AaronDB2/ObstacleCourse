d99368a30ffbad3106c6001d44952ddae845a4f5
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-15 23:27:31 +0200

    Added player movement with the new unity input system and added follow camera using cinemachine

---
+++ Assets/Input Systems/InputMaster.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:037d4043adabbd896eb188b0407fbec41303f26a905d87b15593807895ead65c
+size 9845
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c098b0892277370e73b36301507fb13d84dbd6cc5c586dc5b4daac6d2ef943bd
+size 1278
